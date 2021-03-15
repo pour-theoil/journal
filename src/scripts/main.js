688895eef81f2ba0955c36a737a2b6e3bb121443
Logan Demmy <frank.demmy@gmail.com>
2021-03-15 11:09:03 -0500

    added moods to filter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import { getjournalentry, createPost } from "../data/DataManager.js";
+import { getjournalentry, createPost, usePostCollection } from "../data/DataManager.js";
 import { getJournal } from "./journalentries/journallist.js";
 import { PostEntry } from "./journalentries/journalentry.js";
 import { NavBar } from "./navbar/navbar.js";
@@ -23,8 +23,30 @@ const startJournal = () => {
     showjournalentries();
 }
 
+const showFilteredPosts = (mood) => {
+    //get a copy of the post collection
+    const filteredmood = mood
+    //filter the data
+    const filteredData = usePostCollection().filter(singlePost => {
+        if (singlePost.mood === filteredmood || mood === "AllPosts") {
+            console.log("this is one of the filtered posts", singlePost);
+            return singlePost;
+        }
+    })
+    const postElement = document.querySelector("#journal");
+    postElement.innerHTML = getJournal(filteredData);
+}
 
-const applicationElement =document.querySelector("body");
+
+const showPostEntry = () => { 
+    //Get a reference to the location on the DOM where the nav will display
+    const entryElement = document.querySelector(".formBox");
+    entryElement.innerHTML = PostEntry();
+    
+}
+
+// Section for event listeners
+const applicationElement = document.querySelector("body");
 applicationElement.addEventListener("click", event => {
     console.log("You Clicked on: ", event.target)
 })
@@ -57,21 +79,22 @@ applicationElement.addEventListener("click", event => {
     }
   
     // be sure to import from the DataManager
-    createPost(postObject).then(startJournal());
-        
+    createPost(postObject).then(response => startJournal());
+    document.querySelector("input[name='postTitle']").value = ""
+    document.querySelector("input[name='postTag']").value = "" 
+    document.querySelector("textarea[name='postDescription']").value = ""  
     }
     
   })
 
-const showPostEntry = () => { 
-    //Get a reference to the location on the DOM where the nav will display
-    const entryElement = document.querySelector(".formBox");
-    entryElement.innerHTML = PostEntry();
-    
-}
-
-
-
+applicationElement.addEventListener("change", event => {
+    if (event.target.id === "moodselector") {
+      const currentmood = event.target.value
+      console.log(`User wants to see ${currentmood} mood`)
+      //invoke a filter function passing the year as an argument
+      showFilteredPosts(currentmood);
+    }
+})
 
 showNavBar();
 showPostEntry();
